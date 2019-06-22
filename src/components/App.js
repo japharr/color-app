bfbc9848c3902e0b0f4fe556a9887ff6701aec73
Jaleel Adesina <jtutoff@gmail.com>
2019-06-22 15:17:03 +0100

    plain redux

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import ColorList from "./ui/ColorList";
 import SortMenu from "./ui/SortMenu";
 import AddColorForm from "./ui/AddColorForm";
 
-const App = ({ store }) => {
+const App = () => {
   return (
     <div className="app">
-      <SortMenu store={store} />
-      <AddColorForm store={store} />
-      <ColorList store={store} />
+      <SortMenu />
+      <AddColorForm />
+      <ColorList />
     </div>
   );
 };
